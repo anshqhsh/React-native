1202562f59f6b4039d79fe14e7a16be24e7031a0
joonhyukklab <joonhyuk.klab@gmail.com>
2022-08-05 12:41:36 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import {useCallback} from 'react';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
+
+function HomeScreen({navigation}: HomeScreenProps) {
+  const [modal, showModal] = React.useState(true);
+  const onClick = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
+
+  return (
+    <>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <TouchableHighlight onPress={onClick}>
+          <Text>Home Screen</Text>
+        </TouchableHighlight>
+      </View>
+      {modal && (
+        <Pressable
+          onPress={() => {
+            showModal(_modal => !_modal);
+          }}
+          style={styles.modal}>
+          <Text>Modal</Text>
+          <View style={styles.modalInner}>
+            <Text>Modal</Text>
+          </View>
+        </Pressable>
+      )}
+    </>
+  );
+}
+
+function DetailsScreen({navigation}: DetailsScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>Details Screen</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator(); // 페이지가 위로 쌓인 다는 것
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details">
+          {props => <DetailsScreen {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    ...StyleSheet.absoluteFillObject,
+  },
+  modalInner: {
+    position: 'absolute',
+    backgroundColor: 'orange',
+    top: 40,
+    bottom: 40,
+    right: 40,
+    left: 40,
+    boderRadius: 20,
+  },
+});
+
+export default App;
---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const DetailePage = () => {
+  return (
+    <View>
+      <Text>DetailPage</Text>
+    </View>
+  );
+};
+
+export default DetailePage;
